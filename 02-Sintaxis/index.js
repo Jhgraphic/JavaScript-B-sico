1a3716a01c0c04b7ef17fd49c3e173c000258b52
Jhgraphic <jhgraphic@gmail.com>
2022-09-22 17:46:50 +0900

    Add files via upload

---
+++ 02-Sintaxis/index.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Joan",
+    35,
+    false,
+    new Date(1986, 11, 25),
+    {
+        titulo: "Sapiens. De animales a dioses: Breve historia de la humanidad",
+        autor: "Yuval Noah Harari",
+        fecha: new Date(2015, 10, 7),
+        url: "https://www.amazon.es/dp/8499926223"
+    },
+];
