3ea97623b08a583fc2fa4c498da4bbfa58d933cc
Jhgraphic <jhgraphic@gmail.com>
2022-09-24 14:30:24 +0900

    Add files via upload

---
+++ 03-Estructuras de control/factorial-break.js
@@ -0,0 +1,9 @@
+let factorial = 1
+let num = 1
+while (true) {
+    factorial *= num
+    num++
+    if (num === 11) 
+    break
+}
+console.log(factorial)  // 3628800
---
+++ 03-Estructuras de control/factorial-for.js
@@ -0,0 +1,5 @@
+let factorial = 1
+for (var num = 1; num < 11; num++) {
+    factorial *= num
+}
+console.log(factorial)  // 3628800
