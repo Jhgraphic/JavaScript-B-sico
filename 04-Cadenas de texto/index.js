eaed3e525b28721499a2f64e47969701a230689d
Jhgraphic <jhgraphic@gmail.com>
2022-09-24 15:58:15 +0900

    Add files via upload

---
+++ 04-Cadenas de texto/index.js
@@ -0,0 +1,18 @@
+let nombre = 'Joan'
+let apellido = 'Horrach'
+
+let estudiante = nombre.concat(' ', apellido)
+
+let estudianteMayus = estudiante.toUpperCase()
+
+let estudianteMinus = estudiante.toLowerCase()
+
+let estudianteLength = estudiante.length
+
+let nombreFirst = nombre[0]
+
+let apellidoLast = apellido[apellido.length -1]
+
+let estudianteSinEspacios = estudiante.replace(/ /g, '')
+
+let nombreEstudiante = estudiante.includes(nombre)
