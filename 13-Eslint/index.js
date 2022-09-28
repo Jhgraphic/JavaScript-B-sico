f3a5c444235318171f38c7bc152851c6023edda7
Jhgraphic <jhgraphic@gmail.com>
2022-09-28 16:45:00 +0900

    Add files via upload

---
+++ 13-Eslint/index.js
@@ -0,0 +1,18 @@
+const nombre = "Joan"
+const apellido = "Horrach"
+
+const estudiante = nombre.concat(" ", apellido)
+
+const estudianteMayus = estudiante.toUpperCase()
+
+const estudianteMinus = estudiante.toLowerCase()
+
+const estudianteLength = estudiante.length
+
+const nombreFirst = nombre[0]
+
+const apellidoLast = apellido[apellido.length - 1]
+
+const estudianteSinEspacios = estudiante.replace(/ /g, "")
+
+const nombreEstudiante = estudiante.includes(nombre)
